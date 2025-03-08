77f9fe64dd12c616cf5d25881504dbba3f80c321
DomiMED <lix6727cristal@gmail.com>
2025-03-09 00:40:15 +0200

    New Version

--- assets/contact.js
+++ assets/contact.js
@@ -1,21 +1,27 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the default form submission
+// Initialize EmailJS with your user ID
+emailjs.init("TDdlWhNeiTO9sffMr");
 
-    // Gather form data
-    const formData = {
-        name: document.getElementById('name').value,
-        email: document.getElementById('email').value,
-        message: document.getElementById('message').value
-    };
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent the default form submission
 
-    // Send the data using EmailJS
-    emailjs.send("service_D0Mshj", "template_nggvnqb", formData)
-        .then(function(response) {
-            alert('Your message has been sent successfully!');
-        }, function(error) {
-            alert('There was an error sending your message. Please try again.');
-            console.error('Error:', error);
-        });
+        // Gather form data
+        const formData = {
+            name: document.getElementById('name').value,
+            email: document.getElementById('email').value,
+            message: document.getElementById('message').value
+        };
 
+        // Send the data using EmailJS
+        emailjs.send("service_D0Mshj", "template_ulm4q1t", formData)
+            .then(function(response) {
+                alert('Your message has been sent successfully!');
+            }, function(error) {
+                alert('There was an error sending your message. Please try again.');
+                console.error('Error:', error); // Log the error details
+                console.log('Service ID:', "service_D0Mshj");
+                console.log('Template ID:', "template_ulm4q1t");
+                console.log('Form Data:', formData);
+            });
+    });
 });
-
